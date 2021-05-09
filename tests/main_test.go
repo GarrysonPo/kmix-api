5c9a704063a4c0906082fe719e584be2f8ee2e44
Igor Poyarkov <ig.evg.po@gmail.com>
2021-05-09 17:15:05 +0300

    moved and added new tests to fetching
    Following features added:
    - moved tests to a separate directory;
    - new tests for all fetch recipes.

---
+++ tests/main_test.go
@@ -0,0 +1,37 @@
+package main_test
+
+import (
+	"fmt"
+	"kmix-api/models"
+	"kmix-api/routers"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+var router *mux.Router
+
+func TestMain(m *testing.M) {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dataSourceName := fmt.Sprintf(
+		"%s:%s@/%s",
+		os.Getenv("APP_DB_USERNAME"),
+		os.Getenv("APP_DB_PORT"),
+		os.Getenv("APP_DB_NAME"),
+	)
+	models.InitDB(dataSourceName)
+	defer models.CloseDB()
+
+	router = routers.SetupRouter()
+
+	exitVal := m.Run()
+	os.Exit(exitVal)
+}
--- tests/recipe_test.go
+++ tests/recipe_test.go
@@ -3,47 +3,36 @@ package main_test
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"kmix-api/models"
-	"kmix-api/routers"
-	"log"
 	"net/http"
 	"net/http/httptest"
-	"os"
 	"testing"
-
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 )
-
-var router *mux.Router
-
-func TestMain(m *testing.M) {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	dataSourceName := fmt.Sprintf(
-		"%s:%s@/%s",
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PORT"),
-		os.Getenv("APP_DB_NAME"),
-	)
-	models.InitDB(dataSourceName)
-	defer models.CloseDB()
-
-	router = routers.SetupRouter()
-
-	exitVal := m.Run()
-	os.Exit(exitVal)
-}
 
 func TestFetchAllRecipes(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	req := httptest.NewRequest("GET", "/api/v1/recipes", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+	checkResponseCode(t, http.StatusOK, w.Code)
+}
+
+func TestFetchAllRecipesWithLimit(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	req := httptest.NewRequest("GET", "/api/v1/recipes?limit=3", nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	router.ServeHTTP(w, req)
+	checkResponseCode(t, http.StatusOK, w.Code)
+}
+
+func TestFetchAllRecipesWithOffset(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	req := httptest.NewRequest("GET", "/api/v1/recipes?offset=3", nil)
 	req.Header.Set("Content-Type", "application/json")
 
 	router.ServeHTTP(w, req)
